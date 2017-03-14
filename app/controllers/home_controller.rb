8a8ccf4d5661d0ea40538d53c32522fafedef2da
Pradeep Chauhan <pradeepentrprises@gmail.com>
2017-03-14 17:31:55 +0530

    Flash added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,16 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_tenant!, :only => [ :index ]
 
   def index
-  end
+  	if current_user
+  		if session[:tenant_id]
+  			Tenant.set_current_tenant session[:tenant_id]
+  		else
+  			Tenant.set_current_tenant current_user.tenants.first
+  		end
+  		@tenant = tenant.current_tenant
+  		params[:tenant_id] = @tenant.id
+  		end	
+
+    end
+
 end
